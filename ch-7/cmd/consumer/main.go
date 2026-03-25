5595bd3528c5cfd5bd7dc9d5f6faa56230927666
Lauchlan Toal <lauchlan.toal@redspace.com>
2026-03-25 16:17:48 -0300

    fix: check shutdown error codes from Prometheus endpoint server

--- ch-7/cmd/consumer/main.go
+++ ch-7/cmd/consumer/main.go
@@ -60,7 +60,9 @@ func run() error {
 		shutdownCtx, cancelShutdown := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancelShutdown()
 		log.Println("Shutting down metrics server...")
-		promServer.Shutdown(shutdownCtx)
+		if err := promServer.Shutdown(shutdownCtx); err != nil {
+			log.Printf("Error shutting down metrics server: %v", err)
+		}
 	}()
 
 	rp, err := kgo.NewClient(
